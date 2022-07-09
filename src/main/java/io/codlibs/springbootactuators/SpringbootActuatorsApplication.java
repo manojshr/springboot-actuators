1f894668437c479db1c0065663bc8fc40f5aa983
Manoj Sharma <manoj.mahijpr@gmail.com>
2022-07-09 20:09:48 +0530

    Initial Commit

---
+++ src/main/java/io/codlibs/springbootactuators/SpringbootActuatorsApplication.java
@@ -0,0 +1,13 @@
+package io.codlibs.springbootactuators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActuatorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootActuatorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/codlibs/springbootactuators/SpringbootActuatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codlibs.springbootactuators;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootActuatorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
